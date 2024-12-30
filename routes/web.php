9aee4e9d41c6eb7a6b64afe6eaf70584b85e29d2
codetesla <uoladele99@gmail.com>
2024-12-30 21:25:14 +0100

    vercel integartion web test

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
+use App\Http\Controllers\pdfController;
 
-Route::get("auth/google/callback", [
-  UserController::class,
-  "handleGoogleCallback",
-]);
+Route::post("/register", [UserController::class, "store"]);
+Route::post("/login", [UserController::class, "login"]);
+
+Route::middleware("auth:sanctum")->group(function () {
+  Route::post("/pdfConvert", [PdfController::class, "ConvertPDF"]);
+  Route::get("/fetchPDF", [PdfController::class, "getUserPDFs"]);
+  Route::get("/downloadPDF/{id}", [PdfController::class, "downloadPDF"]);
+  Route::delete("/deletePDF/{id}", [PdfController::class, "deletePDF"]);
+  Route::get("/user", [UserController::class, "getUserInfo"]);
+  Route::get("/hello", [UserController::class, "hello"]);
+});
+Route::get("/auth/google", [UserController::class, "redirectToGoogle"]);
+Route::post("/exchangeToken", [UserController::class, "exchangeToken"]);
