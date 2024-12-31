72cf396bcee287e64dd857b11a8c1ba7bb1eee9f
codetesla <uoladele99@gmail.com>
2024-12-31 07:24:58 +0100

    verceel.json fix

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\userController;
+use App\Http\Controllers\pdfController;
+
+Route::post("/register", [UserController::class, "store"]);
+Route::post("/login", [UserController::class, "login"]);
+
+Route::middleware("auth:sanctum")->group(function () {
+  Route::post("/pdfConvert", [PdfController::class, "ConvertPDF"]);
+  Route::get("/fetchPDF", [PdfController::class, "getUserPDFs"]);
+  Route::get("/downloadPDF/{id}", [PdfController::class, "downloadPDF"]);
+  Route::delete("/deletePDF/{id}", [PdfController::class, "deletePDF"]); 
+  Route::get("/user", [UserController::class, "getUserInfo"]);
+  Route::get("/hello", [UserController::class, "hello"]);
+});
+Route::get("/auth/google", [UserController::class, "redirectToGoogle"]);
+Route::post("/exchangeToken", [UserController::class, "exchangeToken"]);
