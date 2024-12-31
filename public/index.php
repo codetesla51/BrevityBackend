b5d479352ad1a5d566077c2b8bbb9d8573a46eb3
codetesla <uoladele99@gmail.com>
2024-12-31 12:45:26 +0100

    Fixed bugs and resolved issues.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
