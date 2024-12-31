4853d68c8c7304040445bab163935d78c0bdfb70
codetesla <uoladele99@gmail.com>
2024-12-31 07:32:42 +0100

    verceel.json fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
 use Illuminate\Session\Middleware\StartSession;
 return Application::configure(basePath: dirname(__DIR__))
   ->withRouting(
-    web: __DIR__ . "/../routes/web.php",
+    web: __DIR__ . "/../routes/api.php",
     commands: __DIR__ . "/../routes/console.php",
     health: "/up",
     then: function ($router) {
