22aeb5e0332cf83a16f34277e5d9d85aa6d34add
✨ᴜᴛʜᴍᴀɴ • ʙᴜɪʟᴅꜱ • ʙᴀᴄᴋᴇɴᴅꜱ ✨🚀 <149274873+codetesla51@users.noreply.github.com>
2025-01-22 15:29:01 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://ai-brevity.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require __DIR__ . "/../public/index.php";
